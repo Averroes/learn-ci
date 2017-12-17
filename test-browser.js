2104d80aa661298f656eae2e58f9f877fceebf79
digital planets <digplan@users.noreply.github.com>
2017-12-17 15:26:39 -0600

    Update test-browser.js

--- test-browser.js
+++ test-browser.js
@@ -4,45 +4,14 @@ console.log(`Browser?${isBrowser}`, platform)
 ;
 
 (async ()=>{
-  console.log(`
-     .html()
-     .eval(code)
-     .exists(selector) [true|false]
-     .get(url) [object]
-     .click(selector)
-  `)
   console.log(`Attempting a simple test`)
-
-  if(isBrowser){
-   document.write('<script src=http://rawgit.com/digplan/testpage/master/testpage.js >')
-  } else {
-    try {
-      var testpage = require('./testpage')
-    } catch(e) {
-      var testpage = require('testpage')
-    }
-  }
+  var testpage = require('./testpage')
   console.log(`loaded testpage ${typeof testpage} - ok`)
 
   const page = await testpage('https://google.com')
-  const loc = await page.eval('location.href')
-
-  const html = await page.html()
-    console.log(html)
-
-  const e = await page.eval('1+1')
-  const head = html.substring(0, 6)
+  const e = await page.evaluate('1+1')
   console.log(e==2 ? `1 + 1 = ${e} - ok`:`Error cannot eval JS`)
-  console.log(head ? `doc: ${head} - ok`:`Error cannot read document`)
-
-  const el = await page.exists('html')
-  console.log(`Element exists ${el} - ok`)
-  const nonel = await page.exists('somefakeelement')
-  console.log(`Element not exists ${nonel} - ok`)
-
-//  const elc = await page.click('#hpt1 > a:nth-child(1)')
-//  console.log(elc)
-
+  
   const getdata = await page.get('https://restbin-iwgv042975uv.runkit.sh/api')
   console.log(`get(url) - ${getdata.date} ${getdata.time} - ok`)
 
