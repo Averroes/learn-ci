46e9c2965892eabbdca3b130642ec9bca4535df2
digital planets <digplan@users.noreply.github.com>
2017-12-17 15:24:46 -0600

    Update testpage.js

--- testpage.js
+++ testpage.js
@@ -8,23 +8,11 @@ const testpage = url =>new Promise(r=>{
           var j = await f.json()
           return j
         },
-        html: async x=>{
-          var el = 'window.document.documentElement.innerHTML'
-          return (await cli.Runtime.evaluate({expression: el})).result.value
-        },
-        eval: async x=>{
+        evaluate: async x=>{
          var r = await cli.Runtime.evaluate({expression:x})
          if(r.result.subtype=='error') throw Error(r.result.description)
          return r.result.value 
-        },
-        exists: async sel=>new Promise(async (r,j)=>{
-          var el = await cli.Runtime.evaluate({expression: `document.querySelector("${sel}")`})
-          r(el.result.subtype!=='null')
-        }),
-        click: async sel=>new Promise(async r=>{
-          var el = await cli.Runtime.evaluate({expression: `document.querySelector("${sel}").click()`})
-          r(JSON.stringify(el))
-        })
+        }
       })
     }) 
     cli.Network.enable()
