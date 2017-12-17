aa50e1a42bf86e2209010ea12ec11856e3000e9b
digital planets <digplan@users.noreply.github.com>
2017-12-17 16:16:16 -0600

    Update test-browser.js

--- test-browser.js
+++ test-browser.js
@@ -1,20 +1,10 @@
-const isBrowser = typeof window !== 'undefined'
-const platform = isBrowser ? navigator.userAgent : `${require('os').platform} ${process.version}`
-console.log(`Browser?${isBrowser}`, platform)
-;
-
 (async ()=>{
   console.log(`Attempting a simple test`)
-  var testpage = require('./testpage')
-  console.log(`loaded testpage ${typeof testpage} - ok`)
-
-  const page = await testpage('https://google.com')
+  
+  const page = await require('testchrome')('https://google.com')
   const e = await page.evaluate('1+1')
   console.log(e==2 ? `1 + 1 = ${e} - ok`:`Error cannot eval JS`)
-  
-  const ht = await page.evaluate('document.documentElement.outerHTML')
-  console.log('html: ' + ht)
-  
+ 
   const getdata = await page.get('https://restbin-iwgv042975uv.runkit.sh/api')
   console.log(`get(url) - ${getdata.date} ${getdata.time} - ok`)
 
