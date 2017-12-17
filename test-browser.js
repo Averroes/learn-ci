39056b209a9df18fee8b9b1308d1e8daeb7060f1
digital planets <digplan@users.noreply.github.com>
2017-12-17 16:42:03 -0600

    Update test-browser.js

--- test-browser.js
+++ test-browser.js
@@ -6,7 +6,7 @@ try {
   const e = await page.evaluate('1+1')
   console.log(e==2 ? `1 + 1 = ${e} - ok`:`Error cannot eval JS`)
  
-  const getdata = await page.get('https://restbin-iwgv042975uv.runkit.sh/api')
+  const getdata = await page.get('xxx  https://restbin-iwgv042975uv.runkit.sh/api')
   console.log(`get(url) - ${getdata.date} ${getdata.time} - ok`)
 
   process.exit(0) 
